b9c0c1952d2db14b6f94744bf793f5caa5b30f1e
nafies1 <ghuroba.muslim@gmail.com>
2019-10-22 21:39:01 +0700

    instead using nested if else, else if syntax can be used

--- exercise-3.js
+++ exercise-3.js
@@ -2,35 +2,27 @@
 
 // langkah pertama, set variabel yaitu variabel nama dan peran
 var nama = 'Nafies';
-var peran = 'penyulap';
+var peran = 'Penyihir';
 
 // buat pengkondisian menggunakan if else
 	// pengkondisian jika nama kosong
 if (nama ===''){
 	console.log('Nama harus diisi!');
-}else {
+} else {
 	// pengkondisian jika peran kosong
 	if (peran === '') {
 		console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
-	} else{
-			// jika perannya Ksatria
-			if (peran === 'Ksatria') {
-				console.log(`Selamat datang di Dunia Proxytia, ${nama}.`);
-				console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
-			} else{
-				// jika perannya tabib
-				if (peran === 'Tabib') {
-					console.log(`Selamat datang di Dunia Proxytia, ${nama}.`);
-					console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`);
-				} else{
-					// jika perannya Penyihir
-					if (peran === 'Penyihir') {
-						console.log(`Selamat datang di Dunia Proxytia, ${nama}.`);
-						console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
-					} else{
-						console.log('Peran Anda tidak ada di dunia Proxytia. Silahkan pilih peran Ksatria, Tabib, atau Penyihir.');
-					}
-				}
-			}	
-	}
+	} else if (peran === 'Ksatria') { 		// jika perannya Ksatria
+			console.log(`Selamat datang di Dunia Proxytia, ${nama}.`);
+			console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+	} else if (peran === 'Tabib') {    // jika perannya tabib
+			console.log(`Selamat datang di Dunia Proxytia, ${nama}.`);
+			console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`);
+	} else if (peran === 'Penyihir') {     // jika perannya Penyihir
+			console.log(`Selamat datang di Dunia Proxytia, ${nama}.`);
+			console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);			
+	} else {
+			console.log('Peran Anda tidak ada di dunia Proxytia. Silahkan pilih peran Ksatria, Tabib, atau Penyihir.');
+	}				
+	
 }
