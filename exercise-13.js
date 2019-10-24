91405dd74e1a0d9a5f70484e0262a7dd6070b798
nafies1 <ghuroba.muslim@gmail.com>
2019-10-24 14:34:12 +0700

    menyelesaikan exercise-13, membandingkan jumlah x dan o

--- exercise-13.js
+++ exercise-13.js
@@ -2,6 +2,22 @@
 
  function xo(str) {
   // you can only write your code here!
+  var lengthStr = str.length - 1;
+  var ex=0;
+  var oo=0;
+  for (i = lengthStr; i >= 0; i--){
+  	if (str[i] == 'x') {
+  		ex++;
+  	} else if (str[i] == 'o') {
+  		oo++;
+  	} else {
+  		return 'input tidak valid';
+  	}
+  }
+ 
+  var isiXnO = ex==oo;
+  return isiXnO;
+  
 }
 
 // TEST CASES
